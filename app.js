336417d4d98c77bfda5e7948d6eb09ad87aecbfc
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-08-15 15:48:36 +0530

    create listing model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8081;
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -19,4 +20,18 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send("Hi,i am root");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Goa",
+    country: "india",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
 });
