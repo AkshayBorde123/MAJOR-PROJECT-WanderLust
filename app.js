564722402d3acaa87dfb20bc6b773ed81982dd55
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-08-15 14:44:08 +0530

    create sever an d DB

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 8081;
 
-mongoose.connect("mongodb://127.0.0.1:27017/");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 app.listen(port, () => {
   console.log(`app is running on ${port}`);
