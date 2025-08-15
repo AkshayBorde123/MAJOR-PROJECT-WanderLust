d3626bf3a36be97f0c5f67a56cfc117e4f06afad
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-08-15 18:25:08 +0530

    index.ejs is updated

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.get("/", (req, res) => {
   res.send("Hi,i am root");
 });
 
+// ------- sample data-----
+/*
 app.get("/testListing", async (req, res) => {
   let sampleListing = new Listing({
     title: "My New Villa",
@@ -39,8 +41,15 @@ app.get("/testListing", async (req, res) => {
   console.log("sample was saved");
   res.send("successful testing");
 });
+*/
 
 app.get("/listings", async (req, res) => {
   let allListings = await Listing.find();
   res.render("index.ejs", { allListings });
+});
+
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("show.ejs", { listing });
 });
