60c158b41070ac4043522e55c6983d3833d9d2b9
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-08-22 13:31:28 +0530

    custome error handling middleware

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(port, () => {
-  console.log(`app is running on ${port}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Hi,i am root");
 });
@@ -49,42 +45,60 @@ app.get("/testListing", async (req, res) => {
 });
 */
 
+// index route
 app.get("/listings", async (req, res) => {
   let allListings = await Listing.find();
   res.render("./listings/index.ejs", { allListings });
 });
 
+// new route
 app.get("/listings/new", (req, res) => {
   res.render("./listings/new.ejs");
 });
 
-app.post("/listings", async (req, res) => {
-  let newListing = new Listing(req.body.listing);
-  console.log(newListing);
-  await newListing.save();
-  res.redirect("/listings");
+// create route
+app.post("/listings", async (req, res, next) => {
+  try {
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
 });
 
+// show route
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
   res.render("./listings/show.ejs", { listing });
 });
 
+// edit route
 app.get("/listings/:id/edit", async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
   res.render("./listings/edit.ejs", { listing });
 });
 
+// update route
 app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
 });
 
+//  delete roue
 app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
+});
+
+app.use((err, req, res, next) => {
+  res.send("Something went wrong !");
+});
+
+app.listen(port, () => {
+  console.log(`app is running on ${port}`);
 });
